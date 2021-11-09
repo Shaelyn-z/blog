77ab2c31cceb6f08cc576d72d1e3178070cbb8fc
v_lipnzhang <v_lipnzhang@tencent.com>
2021-11-09 09:45:03 +0800

    更新脚本

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -17,9 +17,10 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:shaelyn-z/shaelyn-z.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+# 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
+git push -f git@github.com:Shaelyn-z/shaelynblog.github.io.git master:gh-pages
 
 cd -
